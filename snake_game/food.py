f23abde50ca071009e2ec465d05f9b369451fd7d
Manswi Sati <manvisati03@gmail.com>
2026-02-09 15:24:56 +0530

    Add Food class for random food generation
    Defines the Food class for the snake game, handling food generation and positioning.

---
+++ snake_game/food.py
@@ -0,0 +1,28 @@
+"""
+food.py
+
+This file defines the Food class.
+It handles random food generation and positioning
+within the game window.
+"""
+
+from turtle import Turtle
+import random
+
+
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5,stretch_wid=0.5)
+        self.color("green")
+        self.speed("fastest")
+        self.refresh()
+
+
+    def refresh(self):
+        randomx = random.randrange(-280, 280)
+        randomy = random.randrange(-280, 280)
+        self.goto(randomx, randomy)
