c75fdfecb0bd44541dddefe5f42a4de6e018e4c4
Manswi Sati <manvisati03@gmail.com>
2026-02-10 15:32:32 +0530

    Add Paddle class for vertical movement in Pong game
    This file defines the Paddle class for the Pong game, handling paddle creation and movement.

---
+++ Pong_Game/paddle.py
@@ -0,0 +1,29 @@
+"""
+paddle.py
+
+This file defines the Paddle class.
+It is responsible for creating paddle objects
+and handling their vertical movement in response
+to player input.
+"""
+
+
+from turtle import Turtle, Screen
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
+
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
